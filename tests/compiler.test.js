38881d69598226638eacc291d4c4f67fb3f8c053
starptech <deusdustin@gmail.com>
2019-05-27 21:41:50 +0200

    update deps, use snapshots

--- tests/compiler.test.js
+++ tests/compiler.test.js
@@ -1,16 +1,15 @@
 const SvelteCompiler = require('../index.js');
-const svelteText = require('./svelteStub.js');
-const { outputData } = require('./stubOutput.js');
+const svelteComponent = require('./svelteStub.js');
 
 const args = {
   path: '/test.svelte',
-  data: svelteText
+  data: svelteComponent
 }
 
 test('compiles file data for bar chart example from svelte site', async () => {
   const compiler = new SvelteCompiler({plugins: {}})
   const result = await compiler.compile(args)
-  expect(result.data.includes(outputData)).toBe(true)
+  expect(result.data).toMatchSnapshot();
 });
 
 test('attributes correct source file for bar chart example from svelte site', async () => {
