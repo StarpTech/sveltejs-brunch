416ec44a9877fdfa0c95fda8998bad1ad43e9414
Jess Patton <Jesspu44@gmail.com>
2021-12-04 00:38:02 -0600

    Add regression test
    1) Make sure change to source map path does not break compilation, or attributes for nested file paths.
    2) Clean up logging

--- tests/compiler.test.js
+++ tests/compiler.test.js
@@ -15,7 +15,12 @@ const sourceTestArgs = {
 test('compiles file data for bar chart example from svelte site', async () => {
   const compiler = new SvelteCompiler({plugins: {}})
   const result = await compiler.compile(args)
-    console.log(result);
+  expect(result.data).toMatchSnapshot();
+});
+
+test('compiles nested file data for bar chart example from svelte site', async () => {
+  const compiler = new SvelteCompiler({plugins: {}})
+  const result = await compiler.compile(sourceTestArgs);
   expect(result.data).toMatchSnapshot();
 });
 
@@ -23,6 +28,12 @@ test('attributes correct source file for bar chart example from svelte site', as
   const compiler = new SvelteCompiler({plugins: {}})
   const result = await compiler.compile(args)
   expect(result.map.sources[0]).toBe('/test.svelte')
+});
+
+test('attributes correct source file for sourceTest bar chart example from svelte site', async () => {
+  const compiler = new SvelteCompiler({plugins: {}})
+  const result = await compiler.compile(sourceTestArgs)
+  expect(result.map.sources[0]).toBe('/source/sourceTest.svelte')
 });
 
 test('original file path is preserved in js sourcemap for brunch', async () => {
